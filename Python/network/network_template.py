8e78b17567e2b85346601f1d32742e510a125d73
serega <sergeypikulev01@gmail.com>
2023-08-03 21:40:33 +0300

    Add template client-server.

---
+++ Python/network/network_template.py
@@ -0,0 +1,46 @@
+import socket
+
+
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 6065  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+def server_program():
+        # get the hostname
+        host = socket.gethostname()
+        port = 6065  # initiate port no above 1024
+
+        server_socket = socket.socket()  # get instance
+        # look closely. The bind() function takes tuple as argument
+        server_socket.bind((host, port))  # bind host address and port together
+
+        # configure how many client the server can listen simultaneously
+        server_socket.listen(2)
+        conn, address = server_socket.accept()  # accept new connection
+        print("Connection from: " + str(address))
+        while True:
+            # receive data stream. it won't accept data packet greater than 1024 bytes
+            data = conn.recv(1024).decode()
+            if not data:
+                # if data is not received break
+                break
+            print("from connected user: " + str(data))
+            data = input(' -> ')
+            conn.send(data.encode())  # send data to the client
+
+        conn.close()  # close the connection
---
+++ Python/network/server.py
@@ -0,0 +1,6 @@
+
+import network_template
+
+
+if __name__ == '__main__':
+    network_template.server_program()
