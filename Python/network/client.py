feccb50358e9e0376224b0426fd4c7fb5023c7bb
serega <sergeypikulev01@gmail.com>
2023-08-03 22:52:47 +0300

    Add ConfigHelper and Utils

---
+++ Python/network/client.py
@@ -0,0 +1,4 @@
+import network_template
+
+if __name__ == '__main__':
+    network_template.client_program()
---
+++ Python/network/utils/utils.py
@@ -0,0 +1,9 @@
+# Ver. 0.1
+
+class Utils:
+    version = '0.1'  # class variable shared by all instances
+
+    def __init__(self):
+        self.version = '0.3'  # instance variable unique to each instance
+
+
